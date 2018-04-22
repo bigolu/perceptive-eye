17c932a7bfd9169ff5a3110854a2342f5e05e0c0
Biggie Emmanuel <bigolu96@gmail.com>
2018-04-22 01:16:34 -0400

    done with visual

--- visual.py
+++ visual.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
+big_cube = np.zeros((20, 20, 20), dtype=np.uint32)
 
 def draw_line(start, dest, subplot, size=20, axis='z'):
     increment = [(dest[0]-start[0])/size, (dest[1]-start[1])/size, (dest[2]-start[2])/size]
@@ -33,7 +33,7 @@ def draw_voxel_cube(subplot, cube):
     subplot.voxels(cube, edgecolors="k")
 
 
-def delete_voxel_line(start, dest, subplot, cube, length=50):
+def delete_voxel_line(start, dest, cube, length=50):
     result = []
     dest = np.array(dest)
     start = np.array(start)
@@ -46,14 +46,15 @@ def delete_voxel_line(start, dest, subplot, cube, length=50):
             cube[r[0]][r[1]][r[2]] = 1
 
 
-def get_voxel_map(camera, list_of_points, subplot, cube, length=50):
+def get_voxel_map(camera, list_of_points, length=50):
+    global big_cube
     camera = np.array(camera)
     list_of_points = np.array(list_of_points)
     for j in range(len(list_of_points)):
-        delete_voxel_line(start=camera, dest=list_of_points[j], subplot=subplot, cube=cube)
-    ncube = np.ndarray.flatten(cube)
+        delete_voxel_line(start=camera, dest=list_of_points[j], cube=big_cube)
+    ncube = np.ndarray.flatten(big_cube)
     # the first result (cube) is the 3d cube, the second (ncube) is the 1d array
-    return cube, ncube
+    return ncube
 
 # Here is some example code for how to run it
 '''
